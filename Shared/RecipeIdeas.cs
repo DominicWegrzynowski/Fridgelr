edba6096416829b91e3770e49eb12f001a715acb
Dominic <DominicWegrzynowski@gmail.com>
2023-09-26 16:27:28 -0500

    Start MealIdeas page

---
+++ Shared/RecipeIdeas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fridgelr.Shared
+{
+    public class Idea
+    {
+        public int index { get; set; } //use int instead of guid because of openai
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+    }
+}
